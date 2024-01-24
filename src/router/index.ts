314d619f3f02baa8ab91ad8f399b7d70d1023bdc
Han Wang <hanforwork896@gmail.com>
2024-01-24 23:43:03 +0800

    feat: add home router

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,14 @@ const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue'),
+      meta: {
+        hiddenHeader: true,
+      }
+    },
+    {
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
