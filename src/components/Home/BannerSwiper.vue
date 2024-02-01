60b23211a19aa372d6ed9cea81101c44b5eb4ce0
panduola666 <panduola666@gmail.com>
2024-02-01 23:54:22 +0800

    fix: 首頁 banner RWD 切版

--- src/components/Home/BannerSwiper.vue
+++ src/components/Home/BannerSwiper.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="banner-wrapper position-relative">
+  <div class="banner-wrapper">
+    <swiper-container
+    :pagination="true"
+    :autoplay="false"
+    :loop="true"
+      >
+        <swiper-slide class="d-flex flex-lg-row flex-column align-items-center justify-content-between gap-7 gap-lg-9 px-4 px-lg-9 banner" v-for="i in 2">
+          <div class="text-primary left-box d-flex flex-column align-items-center align-items-lg-start">
+            <p class="fs-2 mb-2">享樂酒店</p>
+            <p class="fs-5">Enjoyment Luxury Hotel</p>
+            <div class="middle-box d-lg-none"></div>
+          </div>
+          <div class="right-box text-white">
+            <div>
+              <h1 class="fs-lg-title fs-1 mb-5 fw-bold">高雄<br/>豪華住宿之選</h1>
+              <h2 class="fs-lg-3 sub-title fw-semibold p-0">我們致力於為您提供無與倫比的奢華體驗與優質服務</h2>
+              <router-link to="" class="btn btn-book rounded-3 p-4 p-lg-7 w-100 d-flex align-items-center justify-content-end gap-3">
+                <span class="fs-lg-5">立即訂房</span>
+                <span class="btn-book-line"></span>
+              </router-link>
+            </div>
+          </div>
+        </swiper-slide>
+      </swiper-container>
+  </div>
+  <!-- <div class="banner-wrapper position-relative">
     <div class="banner d-flex justify-content-between align-items-center px-2_5 px-lg-9">
       <div class="text-primary fw-bold border-bottom border-2 border-gradient">
         <h1 class="ls-2 fs-2 text-heading mb-2 fw-bold">享樂酒店</h1>
         <p class="fs-5 mb-5 s-2">Enjoyment Luxury Hotel</p>
       </div>
-      <!-- 右半邊 -->
       <div class="position-relative z-3">
         <p class="description-block fs-8 fs-lg-title fw-bold text-white mb-6">高雄<br/>豪華住宿之選</p>
         <p class="fs-lg-3 fs-md-5 text-neutral-40">我們致力於為您提供無與line倫比的奢華體驗與優質服務</p>
-        <!-- TODO: 可改用 router link -->
         <div class="cta-btn">
           <p class="btn-line me-4">立即訂房</p>
         </div>
-        <!-- 白色半透明背景 -->
         <div class="square mt-7 mt-lg-0 border-end border-top border-white-100"></div>
       </div>
     </div>
@@ -23,10 +45,13 @@
       <span class="bg-primary-40 d-inline-block pagination-default me-2"></span>
       <span class="bg-primary-40 d-inline-block pagination-default"></span>
     </div>
-  </div>
+  </div> -->
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+export default {
+
+}
 // @ts-ignore
 </script>
 
@@ -47,44 +72,114 @@
   }
 }
 .banner {
+  padding-top: 112px;
+  padding-bottom: 71px;
   @include lg {
     padding-top: 236px;
-    padding-bottom: 130px;
+    padding-bottom: 166px;
   }
   // margin-top: 116px;
   // margin-bottom: 140px;
 }
-.border-gradient {
-  border-bottom-color: linear-gradient(to right, #BE9C7C, $white);
+.left-box{
+  flex-grow: 1;
+  max-width: 636px;
+  @include lg {
+    border-bottom: 2px solid;
+    border-image: linear-gradient(to right, #BE9C7C, #fff) 2;
+  }
 }
-.right-wrapper {
-  height: 116px;
-
-
+.middle-box{
+  height: 83px;
+  width: 2px;
+   background: linear-gradient(to bottom, #BE9C7C, #fff);
 }
-.square {
-  width: calc(100% - 48px);
-  height: 420px;
-  position: absolute;
-  top: 0;
-  left: 48px;
-  border-radius: 80px;
-  z-index: -10;
+.right-box{
+  position: relative;
+  left: 20px;
+  padding: 60px 20px;
+  flex-grow: 1;
+  max-width: 291px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
   background: linear-gradient(rgba($white, 0) 0%, rgba($white, 0.3) 100%);
+  backdrop-filter:blur(20px);
+  border-radius: 80px;
+  border-width: 1px 1px 0px 0px;
+  border-style: solid;
+  border-color: #F5F7F9;
   @include lg {
-    height: 678px;
+  padding: 96px;
+  max-width: 924px;
+  left: 0px;
+  }
+  &>div{
+    position: relative;
+    left: -58px;
+    @include lg {
+      left: -141px;
+  }
   }
 }
-.description-block {
-  margin-top: 96px;
+.sub-title{
+  margin-bottom: 60px;
 }
-.pagination-block {
-  margin-top: 43px;
-  @include lg {
-    margin-top: 130px;
+.btn-book{
+  background: #fff;
+  &-line{
+    display: inline-block;
+    width: 80px;
+    border: 1px solid #000;
+    @include lg {
+    width: 150px;
+    }
+  }
+  &:hover{
+    background: $primary;
+    color: #fff;
   }
 }
-.active {
-  width: 60px;
+.banner-wrapper{
+  --swiper-pagination-bullet-width: 32px;
+  --swiper-pagination-bullet-height: 4px;
+  --swiper-pagination-bullet-border-radius: 0;
+  --swiper-pagination-bullet-inactive-color: #fff;
+  --swiper-pagination-bullet-inactive-opacity: 1;
+  --swiper-pagination-color: #BF9D7D;
 }
+
+// .border-gradient {
+//   border-bottom-color: linear-gradient(to right, #BE9C7C, $white);
+// }
+// .right-wrapper {
+//   height: 116px;
+
+
+// }
+// .square {
+//   width: calc(100% - 48px);
+//   height: 420px;
+//   position: absolute;
+//   top: 0;
+//   left: 48px;
+//   border-radius: 80px;
+//   z-index: -10;
+//   background: linear-gradient(rgba($white, 0) 0%, rgba($white, 0.3) 100%);
+//   @include lg {
+//     height: 678px;
+//   }
+// }
+// .description-block {
+//   margin-top: 96px;
+// }
+// .pagination-block {
+//   margin-top: 43px;
+//   @include lg {
+//     margin-top: 130px;
+//   }
+// }
+// .active {
+//   width: 60px;
+// }
 </style>
