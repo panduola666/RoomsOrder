ee971ac877338febcd633df046c3c67b3b7b9bbd
panduola666 <panduola666@gmail.com>
2024-02-02 02:41:05 +0800

    feat: 首頁串接 API

--- src/components/Home/RoomSwiper.vue
+++ src/components/Home/RoomSwiper.vue
@@ -1,47 +1,129 @@
 <template>
-  <div class="bg-black position-relative py-lg-10">
+  <div class="bg-black position-relative roomSwiper">
     <!-- 橫條背景 -->
-    <img class="bg-horizontal-line position-absolute end-0" src="@/assets/image/desktop/Line3.png" />
-    <div class="container d-lg-flex align-items-end">
-      <!-- TODO: 電腦版不在容器內 -->
-      <swiper-container class="w-lg-50">
-        <swiper-slide class="position-relative">
-          <img src="@/assets/image/desktop/Room1.png" alt="Room1">
-          <div class="pagination-block text-center mb-5 mb-lg-6">
-            <span class="bg-primary d-inline-block pagination-default active me-2"></span>
-            <span class="bg-primary-40 d-inline-block pagination-default me-2"></span>
-            <span class="bg-primary-40 d-inline-block pagination-default me-2"></span>
-            <span class="bg-primary-40 d-inline-block pagination-default"></span>
-          </div>
+    <img
+      class="bg-horizontal-line position-absolute end-0"
+      src="@/assets/image/desktop/Line3.png"
+    />
+    <swiper-container
+      ref="swiper"
+      :pagination="true"
+      :autoplay="true"
+      :loop="true"
+      class="swiper-container start-0 bottom-0 pb-lg-10"
+    >
+      <template v-if="currRoom._id">
+        <swiper-slide v-for="(img, index) in currRoom.imageUrlList" :key="img">
+          <img :src="img" :alt="`詳情圖${index}`" class="" />
         </swiper-slide>
-        <!-- <swiper-slide>Slide 2</swiper-slide> -->
-        <!-- <swiper-slide>Slide 3</swiper-slide> -->
-      </swiper-container>
-      <div>
-        <div class="text-white fw-bold mb-7">
-          <h2 class="fs-2">尊爵雙人房</h2>
-          <p class="title-description">享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢。</p>
-        </div>
-        <div class="text-white fs-3 fw-bold mb-7">NT$ 10,000</div>
-        <div class="cta-btn">
-          <!-- TODO: 按鈕 hover -->
-          <p class="btn-line">查看更多</p>
-        </div>
-        <div class="text-end">
-          <button type="button" class="btn">
-            <img class="me-6" src="@/assets/icons/arrowLeft.svg" alt="arrowLeft">
-          </button>
-          <button type="button" class="btn">
-            <img src="@/assets/icons/arrowRight.svg" alt="arrowRight">
-          </button>
+      </template>
+    </swiper-container>
+
+    <div class="container position-relative z-3 py-lg-10">
+      <!-- TODO: 電腦版不在容器內 -->
+      <!-- 房間圖片 -->
+
+      <div class="row justify-content-end room-box" v-if="currRoom._id">
+        <div class="col-lg-6 d-flex align-items-end">
+          <div class="w-100">
+            <div class="text-white fw-bold mb-7">
+              <h2 class="fs-2">{{ currRoom.name }}</h2>
+              <p class="title-description">{{ currRoom.description }}</p>
+            </div>
+            <div class="text-white fs-3 fw-bold mb-7">NT$ {{ moneyFormat(currRoom.price) }}</div>
+            <div class="cta-btn">
+              <!-- 帶詳情頁面參數 -->
+              <p class="btn-line">查看更多</p>
+            </div>
+            <div class="text-end">
+              <button type="button" class="btn pointer" @click="preRoom">
+                <img class="me-6" src="@/assets/icons/arrowLeft.svg" alt="arrowLeft" />
+              </button>
+              <button type="button" class="btn pointer" @click="nextRoom">
+                <img src="@/assets/icons/arrowRight.svg" alt="arrowRight" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <img class="w-100 position-absolute bottom-0" src="@/assets/image/desktop/Bg.png" />
   </div>
 </template>
-
+<script lang="ts">
+import { mapActions, mapState } from 'pinia'
+import { roomTypeStore } from '../../stores/room'
+import Swiper from 'swiper'
+import { Pagination } from 'swiper/modules'
+export default {
+  data() {
+    return {
+      currIndex: 0,
+      swiper: {}
+    }
+  },
+  mounted() {
+    this.getRoomList()
+  },
+  watch: {
+    currRoom() {
+      this.$nextTick(() => {
+        this.swiper = new Swiper(this.$refs.swiper, {
+          modules: [Pagination],
+          pagination: true,
+          autoplay: true,
+          loop: true
+        })
+        this.swiper.init()
+      })
+    }
+  },
+  computed: {
+    ...mapState(roomTypeStore, ['roomList']),
+    currRoom() {
+      return this.roomList[this.currIndex] || {}
+    }
+  },
+  methods: {
+    ...mapActions(roomTypeStore, ['getRoomList']),
+    nextRoom() {
+      this.currIndex = this.currIndex + 1 > this.roomList.length - 1 ? 0 : this.currIndex + 1
+    },
+    preRoom() {
+      this.currIndex = this.currIndex - 1 < 0 ? this.roomList.length - 1 : this.currIndex - 1
+    }
+  }
+}
+</script>
 <style lang="scss" scoped>
+.roomSwiper {
+  --swiper-pagination-bullet-width: 32px;
+  --swiper-pagination-bullet-height: 4px;
+  --swiper-pagination-bullet-border-radius: 0;
+  --swiper-pagination-bullet-inactive-color: #fff;
+  --swiper-pagination-bullet-inactive-opacity: 1;
+  --swiper-pagination-color: #bf9d7d;
+}
+
+.room-box {
+  @include lg {
+    height: 900px;
+  }
+}
+.swiper-container {
+  width: 100%;
+  height: 300px;
+  @include lg {
+    position: absolute;
+    height: 900px;
+    width: 50%;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+}
 .title-description {
   font-weight: 500;
 }
