a9d436fb317bda165e9a0b4db27d0cd92bfb01d2
RayChen <53119155+RayChen1996@users.noreply.github.com>
2024-02-04 07:04:58 +0800

    Update BookingResult.vue

--- src/views/room/BookingResult.vue
+++ src/views/room/BookingResult.vue
@@ -17,7 +17,7 @@
                 />
               </div>
               <div>
-                <h3>恭喜，Jessica！</h3>
+                <h3>恭喜，{{ data.name }}！</h3>
                 <h3>您已預訂成功</h3>
               </div>
             </div>
@@ -52,20 +52,21 @@
             <div class="py-3"></div>
 
             <div>姓名</div>
-            <span>Jessica Ｗang </span>
+            <span>{{ data.name }} </span>
             <div class="py-3"></div>
 
             <div>手機號碼</div>
-            <span>+886 912 345 678 </span>
+            <span>{{ data.phone }} </span>
             <div class="py-3"></div>
 
             <div>電子信箱</div>
-            <span>jessica@sample.com </span>
+            <span>{{ data.email }} </span>
             <div class="py-3"></div>
           </div>
           <div class="col-md-5 rounded text-black">
             {{ roomInfo }}
-            <NextRoom :room-info="({} as orderData)" />
+            <NextRoom :room-info="_OrderData" />
+            <!-- <NextRoom :room-info="{} as orderData" /> -->
           </div>
         </div>
       </div>
@@ -89,9 +90,40 @@
 </style>
 
 <script setup lang="ts">
+import { ref, watch, onMounted, PropType } from 'vue'
 import NextRoom from '../../components/UserCenter/NextRoom.vue'
+import fetchAPI from '../../mixin/fetchAPI'
 import type { orderData } from '../../interface/order'
+import { type userInfo } from '@/interface/user'
 
-// const props = defineProps(['roomInfo'])
+const _OrderData = ref<Array<PropType<orderData>>>([])
+
 const { roomInfo } = defineProps(['roomInfo'])
+const userData = JSON.parse(localStorage.getItem('user') as string)
+const data = ref<userInfo>({
+  _id: userData._id,
+  name: '',
+  phone: '',
+  birthday: new Date(userData.birthday).toLocaleDateString(),
+  address: {
+    zipcode: userData.address.zipcode,
+    detail: ''
+  },
+  email: '',
+  createdAt: '',
+  updatedAt: ''
+})
+// const _userName = ref<string>('')
+// const _phone = ref<string>('')
+// const _email = ref<string>('')
+
+onMounted(() => {
+  fetchOrderDetail()
+})
+async function fetchOrderDetail() {
+  const res = await fetchAPI(`/api/v1/orders/`, 'GET', '')
+  // console.log(res)
+  const { status } = res
+}
+function
 </script>
