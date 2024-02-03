762bb304f92ece5c3fac7a79c656ca02ab208274
Han Wang <hanforwork896@gmail.com>
2024-02-03 23:15:38 +0800

    feat: add pagination component & adjust router

---
+++ src/components/Common/PagePagination.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav aria-label="pagination">
+    <ul v-if="pages.totalPages > 1" class="pagination">
+
+      <li class="page-item disabled pointer" :class="{ disabled: pages.curPage === 1}">
+        <a class="page-link" @click="emit('change', pages.curPage - 1)">上一頁</a>
+      </li>
+      <!-- TODO: key -->
+      <li v-for="i in pages.totalPages" class="page-item pointer" :key="`page-${i}`">
+        <a class="page-link" @click="emit('change', i)">{{ i }}</a>
+      </li>
+
+      <li class="page-item pointer" :class="{ disabled: pages.curPage ===  pages.totalPages - 1}">
+        <a class="page-link" @click="emit('change', pages.curPage + 1)">下一頁</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { toRefs } from 'vue'
+const emit = defineEmits(['change'])
+const props = defineProps({
+  pages: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+const { pages } = toRefs(props)
+</script>
---
+++ src/stores/pagination.ts
@@ -0,0 +1,48 @@
+import { ref } from 'vue'
+import { defineStore, storeToRefs } from 'pinia'
+import { roomTypeStore } from './room'
+
+export const paginationStore = defineStore('paginationStore', () => {
+
+  const roomStore = roomTypeStore()
+  const { roomList } = storeToRefs(roomStore) // 保持響應
+  console.log('roomList!!', roomList);
+
+  // Default
+  const pages = ref({
+    totalPages: roomList.length,
+    curPage: 1, // 當前頁碼
+    havePrev: false,
+    haveNext: true
+  })
+
+  const turnPage = (page, scrollToTop = true) => {
+    pages.value.curPage = page
+    pages.value.havePrev = page > 1
+    pages.value.haveNext = page < pages.value.totalPages
+    if (scrollToTop) window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  const paginate = (items, curPage = 1, perPage = 4) => {
+    const totalPages = Math.ceil(items.length / perPage)
+    if (curPage > totalPages) {
+      curPage = totalPages
+    }
+
+    const minIndex = (curPage - 1) * perPage
+    const maxIndex = curPage * perPage
+    const itemPaginated = items.slice(minIndex, maxIndex)
+
+    const page = {
+      curPage,
+      totalPages,
+      havePrev: curPage > 1,
+      haveNext: curPage < totalPages
+    }
+
+    pages.value = { ...page }
+    return [...itemPaginated]
+  }
+
+  return { pages, turnPage, paginate }
+})
